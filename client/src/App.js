ff1aba9b5436bad42e0a433d273e2db6ecf72cc5
Haruka Kajitsuka <habichibi0829@gmail.com>
2023-02-20 13:32:52 +1100

    deleted form control file

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,12 @@ import Home from "./components/Home/Home";
 // import { BrowserRouter, Route } from "react-router-dom";
 import LogIn from "./components/LogIn/LogIn";
 import SignUp from "./components/SignUp/SignUp";
-// import FormControl from "./components/FormControl";
+
 
 const App = () => {
   return(
     <div>
-      {/* <FormControl /> */}
-      {/* <BrowserRouter>
-        <Route path="/login" exact component={LogIn} />
-        <Route path="/register" exact component={SignUp} />
-      </BrowserRouter> */}
+      
       <BrowserRouter>
         <Routes>
           <Route path="/login" exact element={<LogIn />} />
