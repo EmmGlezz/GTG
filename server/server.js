5a0047870b10011d9a835eaa3cc7741b88318ae2
EmmGlezz <contact@emilianogonzalez.com>
2023-03-05 00:33:21 +1100

    Cont ollers/Routes folders added & Games API formatted

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const axios = require("axios");
 const User = require('./models/user.model')
 const jwt = require('jsonwebtoken');
+const games_routes = require('./routes/games')
 require('dotenv').config();
 
 app.use(cors());
@@ -46,32 +47,7 @@ app.post("/api/login", async (req, res) => {
     
 })
 
-app.get("/api/games", async (req, res) => {
-    try {
-        const response = await axios.post('https://id.twitch.tv/oauth2/token?client_id=rp12vq3vzr3w42e7lzauu2pvhr2ul8&client_secret=l0jm3jo3ic1abtw20ye47hwbunq5bn&grant_type=client_credentials')
-        const access_token = response.data.access_token
-
-        const data = 'fields name, cover.*, websites.*, genres.*; where rating >= 80 & release_dates.date > 1577231999; limit 30;';
-
-        const config = {
-        method: 'post',
-        url: 'https://api.igdb.com/v4/games',
-        headers: { 
-            'Client-ID': 'rp12vq3vzr3w42e7lzauu2pvhr2ul8', 
-            'Authorization': `Bearer ${access_token}`, 
-            'Content-Type': 'text/plain', 
-            'Cookie': '__cf_bm=oAq7169WV7Qr8Va_OfXq4dxG8GabjyDAupPTV2seA5w-1677674219-0-Af2KbkxX6K6Ryq6ITUpS3NmVL8OHoZ2N4pVbFMHPgRCSVKA8pov/BHTvnQckW2ItCr906OeACrOkVrWpHpCCvvA='
-        },
-        data : data
-        };
-
-        const games = await axios(config)
-
-        res.send(JSON.stringify(games.data))
-    } catch(err){
-        res.json({ status: "error", error: "Failed to connect" })
-    }
-})
+app.use('/api/games', games_routes)
 
 //Connect to database
 mongoose.connect(process.env.MONGO_URI)
