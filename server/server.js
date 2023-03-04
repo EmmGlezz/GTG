253ee5c2da5c237835708f4103ad7d58119145d3
EmmGlezz <contact@emilianogonzalez.com>
2023-03-04 23:11:08 +1100

    Server.js comments deleted

--- server/server.js
+++ server/server.js
@@ -9,32 +9,6 @@ require('dotenv').config();
 
 app.use(cors());
 app.use(express.json())
-// const URL = 'https://id.twitch.tv/oauth2/token?client_id=rp12vq3vzr3w42e7lzauu2pvhr2ul8&client_secret=l0jm3jo3ic1abtw20ye47hwbunq5bn&grant_type=client_credentials'
-
-
-
-    
-    // const data = await axios.post(URL)
-    
-    // axios({
-    //     url: "https://api.igdb.com/v4/games",
-    //     method: 'POST',
-    //     headers: {
-    //         'Accept': 'application/json',
-    //         'Client-ID': 'rp12vq3vzr3w42e7lzauu2pvhr2ul8',
-    //         'Authorization': 'Bearer mxtwqnyppu7b3nfd4drxx0wsw2q42o',
-    //     },
-    //     data: "fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_localizations,game_modes,genres,hypes,involved_companies,keywords,language_supports,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites;"
-    //   })
-    //     .then(response => {
-    //         console.log(response);
-    //     })
-    //     .catch(err => {
-    //         console.error(err);
-    //     });
-    
-
-
 
 
 app.post("/api/register", async (req, res) => {
@@ -77,7 +51,7 @@ app.get("/api/games", async (req, res) => {
         const response = await axios.post('https://id.twitch.tv/oauth2/token?client_id=rp12vq3vzr3w42e7lzauu2pvhr2ul8&client_secret=l0jm3jo3ic1abtw20ye47hwbunq5bn&grant_type=client_credentials')
         const access_token = response.data.access_token
 
-        const data = 'fields name, cover.*, websites.*, genres.*; where rating >= 80 & release_dates.date > 631152000; limit 30;';
+        const data = 'fields name, cover.*, websites.*, genres.*; where rating >= 80 & release_dates.date > 1577231999; limit 30;';
 
         const config = {
         method: 'post',
