fd29e7e44ccc6b9bd618178c450caa67bc37ffed
EmmGlezz <contact@emilianogonzalez.com>
2023-03-17 14:23:34 +1100

    Debug Deployment

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,54 +1,59 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import "./Main.css";
 import Panel from "./Sidebar/Panel";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Sidebar from "./Sidebar/sidebar";
 import { Link } from "react-router-dom";
 
+
+
 export default function Main() {
-  const [gamesRendered, setGamesRendered] = useState([]);
-  const [loading, setLoading] = useState(false);
+
+  const [gamesRendered, setGamesRendered] = useState([])
+  const [loading, setLoading] = useState(false)
+
 
   useEffect(() => {
-    GamesData();
-  }, []);
-
+    GamesData()
+  }, [])
+  
   const GamesData = async () => {
-    setLoading(true);
-    const response = await fetch("/api/games");
-    const data = await response.json();
-    console.log(data);
-    setGamesRendered(data);
-    setLoading(false);
-  };
+    console.log('fetching')
+    setLoading(true)
+    const response = await fetch('/api/games');
+    const data = await response.json()
+    console.log(data)
+    setGamesRendered(data)
+    setLoading(false)
+  }
 
   return (
     <div>
       {loading ? (
-        <div className="loader-container">
-          <img className="logo-anim" src="images/GTG_LOGO.png" alt="logo" />
-          <div className="spinner"></div>
-          <div></div>
-        </div>
+      <div className="loader-container">
+        <img className="logo-anim" src="images/GTG_LOGO.png" alt="logo"/>
+        <div className="spinner"></div>
+        <div></div>
+      </div>
       ) : (
-        <div className="main-wrapper">
-          <div className="Left-app">
-            <Sidebar />
-          </div>
-          <div className="Right-app">
-            <div className="gamesGrid bg-dark">
-              {" "}
-              {gamesRendered.map((game) => {
-                const gameURL = game.cover.url.replace("thumb", "cover_big");
-                return (
-                  <div className="pt1">
-                    <Panel img={gameURL} name={game.name} gameId={game.id} />
-                  </div>
-                );
-              })}
-            </div>
+      <div className="main-wrapper">
+        <div className="Left-app">
+          <Sidebar />
+        </div>
+        <div className="Right-app">
+          <div className="gamesGrid bg-dark">
+            {" "}
+            {gamesRendered.map(game => {
+              const gameURL = game.cover.url.replace("thumb", "cover_big")
+              return (
+                <div className="pt1">
+                  <Panel img={gameURL} name={game.name} gameId={game.id}/>
+                </div>
+              )
+            })}
           </div>
         </div>
+      </div>
       )}
     </div>
   );
--- server/controllers/games.js
+++ server/controllers/games.js
@@ -1,33 +1,33 @@
-const axios = require('axios')
-const getToken = require('./token')
-require('dotenv').config()
-
+const axios = require("axios");
+const getToken = require("./token");
+require("dotenv").config();
 
 const getGamesData = async (req, res) => {
-    const access_token = await getToken()
-    try {
-        const data = 'fields name, cover.*, websites.*, genres.*; where rating >= 60 & release_dates.date > 1577231999; limit 40;';
-    
-        const config = {
-        method: 'post',
-        url: 'https://api.igdb.com/v4/games',
-        headers: { 
-            'Client-ID': `${process.env.CLIENT_ID}`, 
-            'Authorization': `Bearer ${access_token}`, 
-            'Content-Type': 'text/plain',
-            'Cookie': '__cf_bm=oAq7169WV7Qr8Va_OfXq4dxG8GabjyDAupPTV2seA5w-1677674219-0-Af2KbkxX6K6Ryq6ITUpS3NmVL8OHoZ2N4pVbFMHPgRCSVKA8pov/BHTvnQckW2ItCr906OeACrOkVrWpHpCCvvA='
-        },
-        data : data
-        };
+	const access_token = await getToken();
+	try {
+		const data = "fields name, cover.*, websites.*, genres.*; where rating >= 60 & release_dates.date > 1577231999; limit 40;"
 
-        const games = await axios(config)
+		const config = {
+			method: "post",
+			url: "https://api.igdb.com/v4/games",
+			headers: {
+				'Client-ID': `${process.env.CLIENT_ID}`,
+				'Authorization': `Bearer ${access_token}`,
+				'Content-Type': 'text/plain',
+				'Cookie': '__cf_bm=oAq7169WV7Qr8Va_OfXq4dxG8GabjyDAupPTV2seA5w-1677674219-0-Af2KbkxX6K6Ryq6ITUpS3NmVL8OHoZ2N4pVbFMHPgRCSVKA8pov/BHTvnQckW2ItCr906OeACrOkVrWpHpCCvvA='
+			},
+			data: data,
+		};
 
-        res.send(JSON.stringify(games.data))
-    } catch(err) {
-        res.json({ status: "error", error: "Failed to connect" })
-    }
-}
+		const games = await axios(config);
+
+		res.send(JSON.stringify(games.data));
+	} catch (err) {
+        console.log(err)
+		res.json({ status: "error", error: "Failed to connect" });
+	}
+};
 
 module.exports = {
-    getGamesData
-}
+	getGamesData,
+};
